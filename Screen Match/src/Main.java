cc81168a93d4182f5c577290c04624b740df206c
Nathali Herpich <nathaliherpichvalle@gmail.com>
2025-07-11 16:23:26 -0300

    Add files via upload

---
+++ Screen Match/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Filme:Enrolados");
+
+        int anoDeLancamento = 2011;
+        System.out.println("Ano de lançamento : " + anoDeLancamento);
+        boolean includelan = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.0 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        System.out.println("o filme conta a história de uma \nprincesa perdida de 18 anos, com um longo cabelo mágico que anseia para deixar \nsua torre isolada. Contra a vontade da sua mãe, ela aceita a ajuda de um ladrão intruso \npara levá-la ao mundo que ela nunca viu.");
+    }
+}
